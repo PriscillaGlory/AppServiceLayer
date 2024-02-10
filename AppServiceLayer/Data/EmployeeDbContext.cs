b4037cc17e8d03aed2c473dc75207aaefb815573
priscilla glory <priscilla glory@LAPTOP-1L8F4TDI>
2024-02-10 16:21:11 +0530

    Add project files.

---
+++ AppServiceLayer/Data/EmployeeDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AppServiceLayer.Models;
+
+namespace AppServiceLayer.Data
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext (DbContextOptions<EmployeeDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AppServiceLayer.Models.EmpProfile> EmpProfile { get; set; } = default!;
+    }
+}
